291f33c2f58c86bc4a6b0e37d7d48728de0eecea
julessilvestri <jules.sil05@gmail.com>
2024-01-19 11:54:35 +0100

    add item screen

---
+++ AppLoot/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  AppLoot
+//
+//  Created by Jules SILVESTRI on 1/19/24.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
