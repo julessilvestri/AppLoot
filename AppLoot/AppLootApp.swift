e6a458e42f2903209690d5458268a56b8f478f66
julessilvestri <jules.sil05@gmail.com>
2024-01-19 10:33:35 +0100

    Initial Commit

---
+++ AppLoot/AppLootApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLootApp.swift
+//  AppLoot
+//
+//  Created by Jules SILVESTRI on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AppLootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
